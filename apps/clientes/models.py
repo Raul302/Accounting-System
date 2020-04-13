3c78e786c5c5593d7b532d2e15ea5b0e7aa6c4ce
Raul302 <Luis_1547@hotmail.com>
2020-04-13 01:59:40 -0500

    up

---
+++ apps/clientes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Clientes(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 200, blank = False, null = False)
+    apellidos = models.CharField(max_length = 200, blank = False, null = False)
+    edad = models.IntegerField(blank = False, null = False)
+    
+
